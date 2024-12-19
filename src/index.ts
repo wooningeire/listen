1d7c6d4ecdadd768d0e2adff11772f765afeae00
wooningeire <22846982+wooningeire@users.noreply.github.com>
2024-12-19 02:38:34 -0500

    fix: make options param optional

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ export class Listen<E extends EventTarget=any, P extends Parameters<E["addEventL
     ) {}
     
     static for<E extends EventTarget=any, P extends Parameters<E["addEventListener"]>=any>(
-        target: E, type: P[0], handler: P[1], options: P[2],
+        target: E, type: P[0], handler: P[1], options?: P[2],
     ): Listen<E, P> {
         target.addEventListener(type, handler, options);
         return new this(target, type, handler, options);
